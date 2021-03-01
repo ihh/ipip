dec05d6121a0813759e7aeabe6d0bbd24ff8f2b7
Ian Holmes <ihholmes@gmail.com>
2021-02-28 21:28:05 -0800

    first commit

---
+++ js/ipip.js
@@ -0,0 +1,7 @@
+
+const cards =
+      [{ limit: 1,
+         cards: [
+           { html: "<b>Big 5 Personality Test</b><br>Answer the following questions as truthfully as possible" }
+         ] },
+       { html: 'Game Over', priority: -1 }]
